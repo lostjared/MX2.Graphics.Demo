4e3288258d499ae34e731a9e8589ca51bb2b25ad
Jared Bruni <jaredbruni@gmail.com>
2025-12-12 18:09:14 -0800

    removed desktop stuff its only WebAsm now

--- graphics.cpp
+++ graphics.cpp
@@ -1008,62 +1008,7 @@ int main(int argc, char **argv) {
     } catch (mx::Exception &e) {
         std::cerr << e.text() << "\n";
         return EXIT_FAILURE;
-    }
-#else
-    Argz<std::string> parser(argc, argv);
-    parser.addOptionSingle('h', "Display help message")
-        .addOptionSingleValue('p', "assets path")
-        .addOptionDoubleValue('P', "path", "assets path")
-        .addOptionSingleValue('r',"Resolution WidthxHeight")
-        .addOptionDoubleValue('R',"resolution", "Resolution WidthxHeight");
-    Argument<std::string> arg;
-    std::string path;
-    int value = 0;
-    int tw = 960, th = 720;
-    try {
-        while((value = parser.proc(arg)) != -1) {
-            switch(value) {
-                case 'h':
-                case 'v':
-                    parser.help(std::cout);
-                    exit(EXIT_SUCCESS);
-                    break;
-                case 'p':
-                case 'P':
-                    path = arg.arg_value;
-                    break;
-                case 'r':
-                case 'R': {
-                    auto pos = arg.arg_value.find("x");
-                    if(pos == std::string::npos)  {
-                        mx::system_err << "Error invalid resolution use WidthxHeight\n";
-                        mx::system_err.flush();
-                        exit(EXIT_FAILURE);
-                    }
-                    std::string left, right;
-                    left = arg.arg_value.substr(0, pos);
-                    right = arg.arg_value.substr(pos+1);
-                    tw = atoi(left.c_str());
-                    th = atoi(right.c_str());
-                }
-                    break;
-            }
-        }
-    } catch (const ArgException<std::string>& e) {
-        mx::system_err << e.text() << "\n";
-    }
-    if(path.empty()) {
-        mx::system_out << "mx: No path provided trying default current directory.\n";
-        path = ".";
-    }
-    try {
-        MainWindow main_window(path, tw, th);
-        main_window.loop();
-    } catch(const mx::Exception &e) {
-        mx::system_err << "mx: Exception: " << e.text() << "\n";
-        mx::system_err.flush();
-        exit(EXIT_FAILURE);
-    }
+    }  
 #endif
     return 0;
 }
